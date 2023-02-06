4bb31bf1209a4f1faf2744ffc8ba712cae3df041
Ben Scheiner <brmscheiner@gmail.com>
2023-02-06 03:50:15 -0800

    Switch accordion for tabs

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,109 @@
-import Accordion from "../components/Accordion";
 import Page from "../components/Page";
 import Card from "../components/Card";
 import Link from "../components/Link";
+import Tabs, { TabItem } from "../components/Tabs";
 import CopyText from "../components/CopyText";
+
+
+// Consistent content height keeps page from jumping when switching tabs
+const tabContentMinHeight = 700;
+
+const tabItems: TabItem[] = [
+  { label: 'Skills', id: 'skills', default: true, renderContent: () => {
+    return (
+      <div style={{ minHeight: tabContentMinHeight }}>
+      <List
+        title="Experience"
+        items={[
+          {
+            text: "7 years of frontend development",
+          },
+          {
+            text: "3 years of UX design",
+          },
+          {
+            text: "2 years of data analysis",
+          },
+        ]}
+      />
+      <List
+        title="Languages"
+        items={[
+          {
+            text: "Typescript",
+          },
+          {
+            text: "Javascript",
+          },
+          {
+            text: "HTML/CSS",
+          },
+          {
+            text: "Python",
+          },
+        ]}
+      />
+      <List
+        title="Ecosystems"
+        items={[
+          {
+            text: "React",
+          },
+          {
+            text: "Material-UI",
+          },
+          {
+            text: "d3.js",
+          },
+        ]}
+      />
+      <List
+        title="Software"
+        items={[
+          {
+            text: "Figma",
+          },
+        ]}
+      />
+      </div>
+    )
+  } },
+  { label: 'Interests', id: 'interests', renderContent: () => {
+    return (
+      <div style={{ minHeight: tabContentMinHeight }}>
+      <List
+        items={[
+          {
+            text: "Working with non-profits, B Corps and local businesses",
+          },
+          {
+            text: "More design work!",
+          },
+          {
+            text: "Data visualization",
+          },
+        ]}
+      />
+      </div>
+    )
+  } },
+  { label: 'Contact', id: 'contact', renderContent: () => {
+    return (
+      <div style={{ minHeight: tabContentMinHeight }}>
+      <Referral url="https://github.com/brmscheiner" label="Github" />
+      <Referral
+        url="https://www.linkedin.com/in/brmscheiner/"
+        label="LinkedIn"
+      />
+      <Referral
+        url="mailto:brmscheiner@gmail.com"
+        label="brmscheiner@gmail.com"
+        copyable
+      />
+      </div>
+    )
+  } },
+];
 
 function Referral(props: { url: string; label: string; copyable?: boolean }) {
   const { url, label, copyable } = props;
@@ -19,12 +120,12 @@ function Referral(props: { url: string; label: string; copyable?: boolean }) {
   );
 }
 
-interface Item {
+interface ListItem {
   key?: string;
   text: string;
 }
 
-function List(props: { title?: string; items: Item[] }) {
+function List(props: { title?: string; items: ListItem[] }) {
   return (
     <p>
       {props.title}
@@ -41,97 +142,21 @@ export default function About() {
   return (
     <Page title="About">
       <Card>
+        <h3 className="font-bold text-lg my-2">Why Subculture Creations?</h3>
         <p className="mb-6">
-          Ben is a frontend developer and UX designer based in Portland, Oregon. In 2022, he
-          purchased this domain because{" "}
-          <Link external href="https://www.benscheiner.com/" label="benscheiner.com" /> was taken by a different Ben who admittedly seems like a pretty cool guy. This Ben
-          wanted a place on the internet to call his own, where he could ramble
-          freely and put his creations on display.
+          Well, for one, <Link external href="https://www.benscheiner.com/" label="benscheiner.com" /> was taken by a different Ben (who admittedly seems like a pretty cool guy).{" "}
+          But as a lover of racquetball, ultimate frisbee, synthesizers, and so many other fringe activities, I wanted to pay homage to the subculture. I honestly believe that{" "}
+          subcultures are what make life interesting an drive humanity forward. The best ideas are usually found in the margins.
+        </p>
+        <h3 className="font-bold text-lg my-2">Why make a website in the first place?</h3>
+        <p className="mb-6">
+          I am a software engineer and a web designer, so it felt weird not to have a site of my own. Plus I don't really maintain a social media presence, so I wanted a{" "}
+          place to share my thoughts and creations.
         </p>
 
-        <Accordion title="Skills">
-          <List
-            title="Experience"
-            items={[
-              {
-                text: "7 years of frontend development",
-              },
-              {
-                text: "3 years of UX design",
-              },
-              {
-                text: "2 years of data analysis",
-              },
-            ]}
-          />
-          <List
-            title="Languages"
-            items={[
-              {
-                text: "Typescript",
-              },
-              {
-                text: "Javascript",
-              },
-              {
-                text: "HTML/CSS",
-              },
-              {
-                text: "Python",
-              },
-            ]}
-          />
-          <List
-            title="Ecosystems"
-            items={[
-              {
-                text: "React",
-              },
-              {
-                text: "Material-UI",
-              },
-              {
-                text: "d3.js",
-              },
-            ]}
-          />
-          <List
-            title="Software"
-            items={[
-              {
-                text: "Figma",
-              },
-            ]}
-          />
-        </Accordion>
+        <h3 className="font-bold text-lg my-2">More about Ben</h3>
+        <Tabs items={tabItems} />
 
-        <Accordion title="Interests">
-          <List
-            items={[
-              {
-                text: "Working with non-profits, B Corps and local businesses",
-              },
-              {
-                text: "More design work!",
-              },
-              {
-                text: "Data visualization",
-              },
-            ]}
-          />
-        </Accordion>
-        <Accordion title="Contact">
-          <Referral url="https://github.com/brmscheiner" label="Github" />
-          <Referral
-            url="https://www.linkedin.com/in/brmscheiner/"
-            label="LinkedIn"
-          />
-          <Referral
-            url="mailto:brmscheiner@gmail.com"
-            label="brmscheiner@gmail.com"
-            copyable
-          />
-        </Accordion>
       </Card>
     </Page>
   );
